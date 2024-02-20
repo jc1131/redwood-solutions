4c5f178029130c7681126392cc6f812490f99c02
Jordan Crist <jordancrist3@gmail.com>
2024-02-20 22:35:27 +0000

    added fluff

--- models/dynamic-designs/champion_model.sql
+++ models/dynamic-designs/champion_model.sql
@@ -1,36 +1,37 @@
+WITH
+rfm_summary AS (SELECT * FROM {{ ref("stg_rfm") }}),
 
-with rfm_summary as (
-  select * from {{ ref('stg_rfm') }}
+rfm_category AS (SELECT * FROM {{ ref("stg_category") }}),
+
+stg_customer AS (SELECT * FROM {{ ref("stg_customer") }}),
+
+final AS (
+    SELECT
+        rfm_s.customerid AS customer_name,
+        rfm_s.recency AS days_since_last_order,
+        rfm_s.frequency AS number_of_orders,
+        cast(rfm_s.monetary AS numeric) AS total_sales,
+        CASE
+            WHEN rfm_s.monetary < 1000 THEN 'Do Not Call' ELSE
+                rfm_c.rfm_category
+        END AS customer_segment,
+        CASE
+            WHEN rfm_s.monetary < 1000
+                THEN 'Do Not Call'
+            WHEN cust.num_year > 3 AND rfm_s.monetary < 1000
+                THEN 'Loyal'
+            WHEN rfm_s.monetary > 1000 AND rfm_s.monetary < 7500
+                THEN 'Loyal'
+            WHEN rfm_s.monetary > 7500
+                THEN 'Champion'
+            ELSE rfm_c.rfm_category
+        END AS customer_segment_adjusted,
+        rfm_s.r_quartile + 1 AS r_quart,
+        rfm_s.f_quartile + 1 AS f_quart
+    FROM rfm_summary AS rfm_s
+    LEFT JOIN rfm_category AS rfm_c ON rfm_s.rfm_score = rfm_c.rfm_score
+    LEFT JOIN stg_customer AS cust ON rfm_s.customerid = cust.customer_id
 )
-, rfm_category as (
-  select * from {{ ref('stg_category') }}
 
-), stg_customer as (
-  select * from {{ ref('stg_customer') }}
-
-),final as (
-select
-  case
-    when rfm_s.monetary < 1000 then 'Do Not Call'
-    else rfm_c.rfm_category 
-    end as customer_segment
-    ,case 
-    when rfm_s.monetary < 1000 then 'Do Not Call'
-    when cust.num_year > 3 and rfm_s.monetary < 1000 then 'Loyal'
-    when rfm_s.monetary > 1000 and rfm_s.monetary < 10000 then 'Loyal'
-    when rfm_s.monetary > 10000 then 'Champion'
-        else rfm_c.rfm_category 
-    end as customer_segment_adjusted
-  ,rfm_s.customerID as customer_name
-  ,rfm_s.recency as days_since_last_order
-  ,rfm_s.frequency as number_of_orders
-  ,cast(rfm_s.monetary as numeric) as total_sales
-  ,rfm_s.r_quartile + 1 as r_quart
-  ,rfm_s.f_quartile + 1 as f_quart
-from rfm_summary rfm_s
-  left join rfm_category rfm_c 
-    on rfm_c.rfm_score = rfm_s.RFM_Score
-left join stg_customer cust
-    on cust.customer_id = rfm_s.customerID
-)
-select * from final
+SELECT *
+FROM final
