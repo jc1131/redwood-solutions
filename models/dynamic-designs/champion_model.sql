97652915769ae744801b1b6b202de7b993e3600e
Jordan Crist <jordancrist3@gmail.com>
2024-02-20 05:23:22 +0000

    added adjusted segment

--- models/dynamic-designs/champion_model.sql
+++ models/dynamic-designs/champion_model.sql
@@ -5,12 +5,22 @@ with rfm_summary as (
 , rfm_category as (
   select * from {{ ref('stg_category') }}
 
+), stg_customer as (
+  select * from {{ ref('stg_customer') }}
+
 ),final as (
 select
   case
-    when rfm_s.monetary < 500 then 'Do Not Call'
+    when rfm_s.monetary < 1000 then 'Do Not Call'
     else rfm_c.rfm_category 
     end as customer_segment
+    ,case 
+    when rfm_s.monetary < 1000 then 'Do Not Call'
+    when cust.num_year > 3 and rfm_s.monetary < 1000 then 'Loyal'
+    when rfm_s.monetary > 1000 and rfm_s.monetary < 10000 then 'Loyal'
+    when rfm_s.monetary > 10000 then 'Champion'
+        else rfm_c.rfm_category 
+    end as customer_segment_adjusted
   ,rfm_s.customerID as customer_name
   ,rfm_s.recency as days_since_last_order
   ,rfm_s.frequency as number_of_orders
@@ -20,5 +30,7 @@ select
 from rfm_summary rfm_s
   left join rfm_category rfm_c 
     on rfm_c.rfm_score = rfm_s.RFM_Score
+left join stg_customer cust
+    on cust.customer_id = rfm_s.customerID
 )
 select * from final
