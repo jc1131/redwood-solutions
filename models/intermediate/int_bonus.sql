e0e3b25f60be19de9367b05f1221633e6c70554d
Jordan Crist <jordancrist3@gmail.com>
2025-01-30 21:38:34 +0000

    Added bonus activity

--- models/intermediate/int_bonus.sql
+++ models/intermediate/int_bonus.sql
@@ -1,54 +1,99 @@
 WITH base_response AS (
     SELECT * FROM {{ ref('int_response_combined') }}
 ),
-config_commission_relationship as (
-    select * from {{ ref('stg_commission_form__config_commission_relationship') }}
-),dim_date as (
-    select * from {{ ref('dim_date') }}
-),activity_bonus_date as (
-     SELECT month_name, month_end_date,next_pay_date
-FROM dim_date
-WHERE year_number = EXTRACT(YEAR FROM CURRENT_DATE())
-qualify row_number() over (partition by month_name, month_end_date order by next_date_day desc) = 1
-order by 2 asc
-)
-bonus_configuration as (
-    select * from {{ ref('stg_commission_form__config_bonus') }}
+
+base_activity as (
+    select * from {{ ref('stg_commission_form__form_activity')}}
 ),
-secondary_bonus as (
-select 
-{{ dbt_utils.generate_surrogate_key(['config_commission_relationship.config_commission_relationship_pk', 'job_order_number']) }} as secondary_bonus_pk
-,config_commission_relationship.secondary_recruiter_name AS recruiter_name
-,DATE_ADD(work_start_date, INTERVAL commission_hold_days DAY) AS bonus_pay_date
-,config_commission_relationship.commission_rate * invoice_amount as bonus_amount
-,CONCAT(
-        FORMAT('%g', config_commission_relationship.commission_rate * 100), '% commission from ', 
-        config_commission_relationship.primary_recruiter_name, 
-        ' on Job Order #', job_order_number, 
-        ' (Start Date: ', work_start_date, 
-        ', Hold Days: ', commission_hold_days
-)AS bonus_description
 
-from base_response
-    left join config_commission_relationship on config_commission_relationship.primary_recruiter_email = base_response.recruiter_email
+config_commission_relationship AS (
+    SELECT *
+    FROM {{ ref('stg_commission_form__config_commission_relationship') }}
+),
 
-where config_commission_relationship.secondary_recruiter_email is not null
+dim_date AS (
+    SELECT * FROM {{ ref('dim_date') }}
+),
 
-), sale_ytd as (
-select 
-form_response_combine_pk
-,due_date
-,recruiter_email
-,invoice_amount
-,sum(invoice_amount) over (partition by recruiter_email order by due_date asc) as total_sales_ytd
-from base_response
+activity_bonus_date AS (
+    SELECT
+        month_name,
+        month_end_date,
+        next_pay_date
+    FROM dim_date
+    WHERE year_number = EXTRACT(YEAR FROM CURRENT_DATE())
+    QUALIFY
+        ROW_NUMBER()
+            OVER (
+                PARTITION BY month_name, month_end_date
+                ORDER BY next_date_day DESC
+            )
+        = 1
+    ORDER BY 2 ASC
+),activity_bonus as (
+    select
+    base_activity.form_activity_pk
+    ,base_activity.activity_bonus_recipient
+    ,activity_bonus_date.next_pay_date as bonus_pay_date
+    ,500 as bonus_amount
+    ,CONCAT(activity_bonus_date.month_name, ' activity bonus') as bonus_description
+    from base_activity
+        left join activity_bonus_date on activity_bonus_date.month_name = base_activity.activity_bonus_month
+),
+
+bonus_configuration AS (
+    SELECT * FROM {{ ref('stg_commission_form__config_bonus') }}
+),
+
+secondary_bonus AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['config_commission_relationship.config_commission_relationship_pk', 'job_order_number']) }}
+            AS secondary_bonus_pk,
+        config_commission_relationship.secondary_recruiter_name
+            AS recruiter_name,
+        DATE_ADD(work_start_date, INTERVAL commission_hold_days DAY)
+            AS bonus_pay_date,
+        config_commission_relationship.commission_rate
+        * invoice_amount AS bonus_amount,
+        CONCAT(
+            FORMAT('%g', config_commission_relationship.commission_rate * 100),
+            '% commission from ',
+            config_commission_relationship.primary_recruiter_name,
+            ' on Job Order #', job_order_number,
+            ' (Start Date: ', work_start_date,
+            ', Hold Days: ', commission_hold_days
+        ) AS bonus_description
+
+    FROM base_response
+    LEFT JOIN
+        config_commission_relationship
+        ON
+            base_response.recruiter_email
+            = config_commission_relationship.primary_recruiter_email
+
+    WHERE config_commission_relationship.secondary_recruiter_email IS NOT null
+
+),
+
+sale_ytd AS (
+    SELECT
+        form_response_combine_pk,
+        due_date,
+        recruiter_email,
+        invoice_amount,
+        SUM(invoice_amount)
+            OVER (PARTITION BY recruiter_email ORDER BY due_date ASC)
+            AS total_sales_ytd
+    FROM base_response
 
 
-),combine_bonus_sale as (
- select config_bonus_pk,
-        bonus_configuration.employee_email as recruiter_email,
-                bonus_configuration.employee_name as recruiter_name,
+),
 
+combine_bonus_sale AS (
+    SELECT
+        config_bonus_pk,
+        bonus_configuration.employee_email AS recruiter_email,
+        bonus_configuration.employee_name AS recruiter_name,
         bonus_configuration.bonus_plan,
         bonus_configuration.bonus_threshold,
         bonus_configuration.bonus_amount,
@@ -56,47 +101,45 @@ from base_response
         bonus_configuration.bonus_end_date,
         base_response.due_date,
         SUM(base_response.invoice_amount) OVER (
-            PARTITION BY bonus_configuration.employee_email, bonus_configuration.bonus_start_date, bonus_configuration.bonus_end_date
+            PARTITION BY
+                bonus_configuration.employee_email,
+                bonus_configuration.bonus_start_date,
+                bonus_configuration.bonus_end_date
             ORDER BY base_response.due_date ASC
             ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
-        ) AS bonus_ytd,
-        
-from bonus_configuration
-    join base_response on base_response.recruiter_email = bonus_configuration.employee_email
-    and base_response.due_date between bonus_configuration.bonus_start_date and bonus_configuration.bonus_end_date
-),primary_bonus as (
-    select
-    config_bonus_pk as primary_bonus_pk
-    ,recruiter_name
-    ,case 
-        when bonus_ytd >= bonus_threshold then due_date
-        else bonus_end_date
-        end as bonus_pay_date
-    ,case 
-        when bonus_ytd >= bonus_threshold then bonus_amount
-        else 0
-        end as bonus_amount
-    ,CONCAT(bonus_plan, ' payout') as bonus_description
-    from combine_bonus_sale
-qualify ROW_NUMBER() OVER (
-     PARTITION BY config_bonus_pk
-     ORDER BY bonus_pay_date asc) = 1 
+        ) AS bonus_ytd
+
+    FROM bonus_configuration
+    INNER JOIN base_response ON
+        bonus_configuration.employee_email = base_response.recruiter_email
+        AND base_response.due_date BETWEEN bonus_configuration.bonus_start_date AND bonus_configuration.bonus_end_date
 ),
 
-retro_bonus as (
-    select
-    *
-    
-    from bonus_configuration
-        left join primary_bonus on    primary_bonus.primary_bonus_pk = bonus_configuration.config_bonus_pk
+primary_bonus AS (
+    SELECT
+        config_bonus_pk AS primary_bonus_pk,
+        recruiter_name,
+        CASE
+            WHEN bonus_ytd >= bonus_threshold THEN due_date
+            ELSE bonus_end_date
+        END AS bonus_pay_date,
+        CASE
+            WHEN bonus_ytd >= bonus_threshold THEN bonus_amount
+            ELSE 0
+        END AS bonus_amount,
+        CONCAT(bonus_plan, ' payout') AS bonus_description
+    FROM combine_bonus_sale
+    QUALIFY ROW_NUMBER() OVER (
+        PARTITION BY config_bonus_pk
+        ORDER BY bonus_pay_date ASC
+    ) = 1
 ),
-    
-    final as (
-    select * from secondary_bonus
-    union all 
-    select * from primary_bonus
-    -- union all 
-    -- select * from retro_bonus
+final AS (
+    SELECT * FROM secondary_bonus
+    UNION ALL
+    SELECT * FROM primary_bonus
+    UNION ALL
+    select * from activity_bonus
 )
-select * from retro_bonus 
 
+SELECT * FROM final
