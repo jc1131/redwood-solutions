1d33366e6aa8bb0d0ca6178aa6cb299e15a2a931
Jordan Crist <jordancrist3@gmail.com>
2025-02-02 19:51:30 +0000

    updated formatting added primary key

--- models/intermediate/int_draw.sql
+++ models/intermediate/int_draw.sql
@@ -1,63 +1,90 @@
-with base_commission as (
-    select * from {{ ref('int_commission') }}
+WITH base_commission AS (
+    SELECT * FROM {{ ref('int_commission') }}
 ),
-config_draw as (
-    select * from {{ ref('stg_commission_form__config_draw') }}
+
+config_draw AS (
+    SELECT * FROM {{ ref('stg_commission_form__config_draw') }}
 ),
-dim_date as (
-    select * from {{ ref('dim_date')}}
+
+dim_date AS (
+    SELECT * FROM {{ ref('dim_date') }}
 ),
-pay_period as (
-    select distinct
-    pay_date
-    from dim_date
-    WHERE year_number = EXTRACT(YEAR FROM CURRENT_DATE())
-    and date_day <= CURRENT_DATE()
+
+pay_period AS (
+    SELECT DISTINCT pay_date
+    FROM dim_date
+    WHERE
+        year_number = EXTRACT(YEAR FROM CURRENT_DATE())
+        AND date_day <= CURRENT_DATE()
 
 )
 ,
-commission_pay_period as (
-select 
-    recruiter_name as employee_name
-    ,pay_date
-    ,sum(tier_commission) as commission_amount
+commission_pay_period AS (
+    SELECT
+        config_draw.config_draw_pk,
+        recruiter_name AS employee_name,
+        pay_date,
+        SUM(tier_commission) AS commission_amount
 
-from base_commission
-    join config_draw on config_draw.employee_name = base_commission.recruiter_name
-    left join dim_date on dim_date.date_day = base_commission.due_date
-group by all
-),draw_pay_period as (
-select 
-employee_name
-,pay_period.pay_date
-,draw_amount * - 1 as draw_amount
-,'Semi-monthly draw' as draw_description
+    FROM base_commission
+    INNER JOIN
+        config_draw
+        ON base_commission.recruiter_name = config_draw.employee_name
+    LEFT JOIN dim_date ON base_commission.due_date = dim_date.date_day
+    GROUP BY ALL
+),
 
-from config_draw
-join pay_period on 1=1
-union all 
+draw_pay_period AS (
+    SELECT
+        config_draw.config_draw_pk,
+        employee_name,
+        pay_period.pay_date,
+        draw_amount * -1 AS draw_amount,
+        'Semi-monthly draw' AS draw_description
 
-select
-employee_name
-,config_draw.draw_start_date
-,balance_owed * -1 as draw_amount
-,'Balance forward' as draw_description
-from config_draw
-join dim_date on dim_date.date_day = config_draw.draw_start_date
+    FROM config_draw
+    INNER JOIN pay_period ON 1 = 1
+    UNION ALL
+
+    SELECT
+        config_draw.config_draw_pk,
+        employee_name,
+        config_draw.draw_start_date,
+        balance_owed * -1 AS draw_amount,
+        'Balance forward' AS draw_description
+    FROM config_draw
+    INNER JOIN dim_date ON config_draw.draw_start_date = dim_date.date_day
+),
+
+pay_period_amount AS (
+
+    SELECT
+        draw_pay_period.config_draw_pk,
+        draw_pay_period.employee_name,
+        draw_pay_period.pay_date,
+        draw_pay_period.draw_amount,
+        commission_pay_period.commission_amount,
+        draw_pay_period.draw_description,
+        SUM(
+            draw_pay_period.draw_amount
+            + COALESCE(commission_pay_period.commission_amount, 0)
+        )
+            OVER (
+                PARTITION BY draw_pay_period.employee_name
+                ORDER BY draw_pay_period.pay_date
+                ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+            ) AS balance_amount
+    FROM draw_pay_period
+    LEFT JOIN commission_pay_period ON
+        draw_pay_period.pay_date = commission_pay_period.pay_date
+        AND draw_pay_period.employee_name = commission_pay_period.employee_name
+),
+
+pk_generation AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['config_draw_pk', 'pay_date']) }} AS draw_pk,
+        *
+    FROM pay_period_amount
 )
-,pay_period_amount as (
 
-select
-    draw_pay_period.employee_name
-    ,draw_pay_period.pay_date
-    ,draw_pay_period.draw_amount
-    ,commission_pay_period.commission_amount
-    , SUM(draw_pay_period.draw_amount + COALESCE(commission_pay_period.commission_amount, 0)) 
-        OVER (PARTITION BY draw_pay_period.employee_name ORDER BY draw_pay_period.pay_date 
-              ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as balance_amount
-    ,draw_pay_period.draw_description
-from draw_pay_period
-    left join commission_pay_period on commission_pay_period.pay_date = draw_pay_period.pay_date
-    and  commission_pay_period.employee_name = draw_pay_period.employee_name
- )
-select * from pay_period_amount
+SELECT * FROM pk_generation
