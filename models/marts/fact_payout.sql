91cbe0b3a4c425f446e95f360901017b77ee54ec
Jordan Crist <jordancrist3@gmail.com>
2025-03-31 03:24:15 +0000

    added models and test

--- models/marts/fact_payout.sql
+++ models/marts/fact_payout.sql
@@ -2,10 +2,17 @@ with payout_combined as (
     select * from {{ ref('int_payout_combined') }}
 ),dim_date as (
     select * from {{ ref('dim_date') }}
+),
+form_header as (
+    select * from {{ ref('int_response_header')}}
 )
 select
     payout_combined.commission_pk			
-    ,payout_combined.form_response_fk			
+    ,payout_combined.form_response_fk		
+    ,form_header.last_modified
+    ,form_header.job_order_number
+    ,form_header.client_name
+    ,form_header.candidate_name	
     ,payout_combined.recruiter_name			
     ,payout_combined.invoice_payment_date			
     ,payout_combined.commission_pay_date	
@@ -19,3 +26,4 @@ select
 
 from payout_combined
     left join dim_date on dim_date.date_day = payout_combined.commission_pay_date
+    left join form_header on form_header.form_response_pk = payout_combined.form_response_fk
