5f5c759159707b2c40187e49c70b935905649a6e
Jordan Crist <jordancrist3@gmail.com>
2025-04-10 02:11:58 +0000

    added it bonus

--- models/intermediate/source_intermediate/int_bonus_commission.sql
+++ models/intermediate/source_intermediate/int_bonus_commission.sql
@@ -1,7 +1,13 @@
 WITH base_commission AS (
     SELECT * FROM {{ ref('int_commission') }}
-), bonus_configuration AS (
+), base_bonus as (
     SELECT * FROM {{ ref('stg_commission_form__config_bonus') }}
+),
+bonus_it AS (
+    SELECT * FROM base_bonus where bonus_plan in ('Quarter IT Assistance')
+), 
+bonus_configuration AS (
+    SELECT * FROM base_bonus where bonus_plan in ('Annual Sales Goal', 'Quarter Sales Goal')
 ), bonus_agg AS (
     SELECT 
         b.employee_name,
@@ -18,7 +24,7 @@ WITH base_commission AS (
         AND s.due_date BETWEEN b.bonus_start_date AND b.bonus_end_date
     GROUP BY 
         all
-), final as (
+), sale_bonus as (
 
 SELECT 
     employee_name,
@@ -44,6 +50,36 @@ SELECT
         END
     ) AS bonus_description
 FROM bonus_agg
+),it_bonus as (
+    select 
+employee_name,
+    config_bonus_pk,
+    bonus_plan,
+    bonus_start_date,
+    bonus_end_date,
+    null bonus_threshold,
+    bonus_amount,
+    null as total_sales,
+    bonus_amount as payout_amount,
+    'quarterly bonus for IT Assistance' AS bonus_description
+
+ from bonus_it
+where bonus_end_date < current_date()
+),
+
+final as (
+    select
+        *
+    from sale_bonus
+    where payout_amount > 0
+
+    union all
+
+    select
+    *
+    from it_bonus
+
+
 )
 select * from final
-where payout_amount > 0
+
